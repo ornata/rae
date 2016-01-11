766045c61ecdbe2a140c308bb13e1648da1c58d0
Jessica Paquette <jlpaquet@uvic.ca>
2016-01-10 22:24:50 -0800

    put a basic ray tracer in the main

--- main.cpp
+++ main.cpp
@@ -1,20 +1,51 @@
 #include <iostream>
 #include <ctime>
+#include <vector>
 #include "vec.hpp"
 #include "onb.hpp"
 #include "dynarray.hpp"
 #include "rng.hpp"
 #include "shape.hpp"
+#include "img.hpp"
+
+#define WIDTH 500
+#define HEIGHT 500
 
 int main(void)
 {
-    vec3 v0(0.132, 1, 4);
-    vec3 v1(2, 3, 5);
-    vec3 v2(10, 4, 4);
-    rgb c(255,0,0);
-    float r = 1;
-    triangle t(v0,v1,v2,c);
-    sphere s(v1, r, c);
-    std::cout << t << "\n";
-    std::cout << s << "\n";
+    img image(WIDTH, HEIGHT);
+    
+    bool isHit;
+    float tmax;
+    vec3 dir(0,0,-1); // direction of viewing rays
+    hitRecord rec;
+    std::vector<shape*> shapes;
+
+    shapes.push_back(new sphere (vec3(250,250,-1000), 150.0f, rgb(0.8,0.0,0.0)));
+
+    // iterate over every pixel, firing off rays at objects
+    for (int i = 0; i < WIDTH; i++) {
+        for (int j = 0; j < HEIGHT; j++) {
+            tmax = 100000.0f;
+            isHit = false;
+            ray r(vec3(i, j, 0), dir);
+
+            for (auto s : shapes) {
+                if (s->hit(r, 0.0001f, tmax, 0, rec)) {
+                    tmax = rec.t;
+                    isHit = true;
+                }
+
+                if (isHit == true) {
+                    image.set(i, j, rec.colour);
+                }
+
+                else {
+                    image.set(i, j, rgb(0.0, 0.0, 0.0));
+                }
+            }
+        }
+    }
+
+    image.writePPM(std::cout);
 }
