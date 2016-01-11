15fdebfa1d14a235d8c2c9bbdf84918f9bd19403
Jessica Paquette <jlpaquet@uvic.ca>
2016-01-10 20:33:32 -0800

    added shapes to main

--- main.cpp
+++ main.cpp
@@ -4,11 +4,17 @@
 #include "onb.hpp"
 #include "dynarray.hpp"
 #include "rng.hpp"
+#include "shape.hpp"
 
 int main(void)
 {
-    rng r;
-    std::cout << r() << "\n";
-    std::cout << r() << "\n";
-    std::cout << r() << "\n";
+    vec3 v0(0.132, 1, 4);
+    vec3 v1(2, 3, 5);
+    vec3 v2(10, 4, 4);
+    rgb c(255,0,0);
+    float r = 1;
+    triangle t(v0,v1,v2,c);
+    sphere s(v1, r, c);
+    std::cout << t << "\n";
+    std::cout << s << "\n";
 }
