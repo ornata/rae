347dfbbdf30a20f85d6e5016a7bc11ed75a8cd9d
Jessica Paquette <jlpaquet@uvic.ca>
2016-01-10 17:26:20 -0800

    added random number generator

---
+++ rng.hpp
@@ -0,0 +1,25 @@
+#ifndef _RNG_H_
+#define _RNG_H_
+
+#include <cstdint>
+#include <cfloat>
+
+struct rng
+{
+    uint64_t seed, mult;
+
+    rng(uint64_t seed_ = 7564231)
+    {
+        seed = seed_;
+        mult = 62089911;
+
+    }
+
+    float operator()()
+    {
+        seed *= mult;
+        return float(seed % UINT64_MAX) / FLT_MAX;
+    }
+};
+
+#endif
