ee2d8d496026b271f151fd96c0de824ec691b57f
Jessica Paquette <jlpaquet@uvic.ca>
2016-01-17 18:22:29 -0800

    added triangle mesh structs

--- shape.hpp
+++ shape.hpp
@@ -9,6 +9,7 @@
 
 #include <cmath>
 #include <iostream>
+#include <cstdint>
 #include "ray.hpp"
 #include "vec.hpp"
 
@@ -24,6 +25,7 @@ struct hitRecord
 /* shape: all shapes will be iterated over and checked for hit and shadowhit */
 struct shape
 {
+    bool mirror = false;
     virtual bool hit(const ray &r, float tmin, float tmax, float time, hitRecord &record) const=0;
     virtual bool shadowHit(const ray &r, float tmin, float tmax, float time) const=0;
 };
@@ -39,6 +41,37 @@ struct triangle : public shape
     triangle(const vec3 &p0_, const vec3 &p1_, const vec3& p2_, const rgb &colour_);
     bool hit(const ray &r, float tmin, float tmax, float time, hitRecord &record) const;
     bool shadowHit(const ray &r, float tmin, float tmax, float time) const;
+};
+
+/*
+* A vertex read from a .mesh file.
+*/
+struct meshVertex {
+    vec3 coords;     // Position of the vertex 
+    vec2 texCoord;   // Map for the texture components
+    vec3 normal;      // Normal of the vertex
+};
+
+/*
+* Triangle in a mesh from a .mesh file. Note that triangles are
+* stored in CW order.
+*/
+struct meshTriangle {
+    uint32_t i0; // ID of first vertex in vertexArray
+    uint32_t i1; // ID of second vertex in vertexArray     
+    uint32_t i2; // ID of third vertex in vertexArray
+};
+
+struct triangleMesh : public shape
+{
+    uint32_t nv;                  // number of vertices
+    uint32_t nt;                  // number of triangles
+    meshVertex* vertexArray;      // contains every vertex in the mesh
+    meshTriangle* triangleArray;  // contains every triangle in the mesh
+
+    triangleMesh(std::istream meshFile);
+    virtual bool hit(const ray &r, float tmin, float tmax, float time, hitRecord &record) const=0;
+    virtual bool shadowHit(const ray &r, float tmin, float tmax, float time) const=0;
 };
 
 /* sphere: defined by a centre and a radius */
